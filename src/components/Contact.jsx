7e73e13bc3f47425cf3a4af60cb2c8f6c9d5170a
wongv <val.wong@netscout.com>
2022-12-14 18:00:44 -0600

    change contact form submission

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ function Contact() {
                   className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                />
                <button
-                  type="button"
+                  type="submit"
                   className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-pink-500 drop-shadow-md hover:stroke-white"
                >
                   send message
